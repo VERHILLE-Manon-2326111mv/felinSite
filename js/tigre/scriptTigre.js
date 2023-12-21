0461b90cc436f5ebe168f256239ead7c05a15aa0
Manon VERHILLE <145963847+VERHILLE-Manon-2326111mv@users.noreply.github.com>
2023-12-21 08:50:13 +0100

    coucou

---
+++ js/tigre/scriptTigre.js
@@ -0,0 +1,12 @@
+// JavaScript code for the web page
+let pageTitle = 'Les Félins - Tigres';
+let introduction = 'Les tigres, ou bien Panthera tigris, sont des animaux sublimes faisant partie des grands félins.';
+let imageUrl = 'images/tigreImage.jpg';
+
+function displayTigerInfo() {
+  console.log(pageTitle);
+  console.log(introduction);
+  console.log(imageUrl);
+}
+
+displayTigerInfo();
